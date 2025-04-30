4ca8c3a14053c96045b382c6e89eeb74c12b7f04
gkfat <gkgkdesign@gmail.com>
2025-04-30 16:25:42 +0800

    增加 API list

--- src/api-list.ts
+++ src/api-list.ts
@@ -1,1 +1,6 @@
-export const API_LIST = ['https://gk-app-client.onrender.com', 'https://gk-app-server.onrender.com/api/v1/health'];
+export const API_LIST = [
+    'https://gk-app-client.onrender.com',
+    'https://gk-app-server.onrender.com/api/v1/health',
+    'https://todoless-client.onrender.com',
+    'https://todoless-server.onrender.com/api/v1/health',
+];
